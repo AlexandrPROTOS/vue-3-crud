a6edb03ea47a94fb3fd1e4a37041119eac974a5d
Abdurkaev Asadullah <a.abdurkaev@gmail.com>
2024-05-17 12:10:24 +0300

    base changes.main js fix

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/vue-3-crud/',
   plugins: [
     vue(),
     AutoImport({
