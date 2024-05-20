ba02a3cc5ddc879be10176578fb0b3d4e940e01a
AsadullahTM <137809335+AsadullahTM@users.noreply.github.com>
2024-05-20 23:20:13 +0300

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -4,14 +4,6 @@ import Posts from '../views/PostsView.vue';
 import TaskDetails from '../views/TaskDetailsView.vue';
 import Tasks from '../views/TasksView.vue';
 
-// export default createRouter({
-//   history: createWebHistory('/vue-3-crud'),
-//   routes: [],
-// });
-
-// import.meta.env.BASE_URL
-
-
 const router = createRouter({
   history: createWebHashHistory('/vue-3-crud'),
   routes: [
